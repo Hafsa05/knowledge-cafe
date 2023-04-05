ed33bed9928f9e0b8396b2d7c43c2e929780abdd
Hafsa05 <hafsa@g.bracu.ac.bd>
2023-04-05 17:10:02 +0600

    app.jsx modification done

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,57 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-// import Header from './components/Header/Header'
-// import Home from './components/Home/Home'
-// import SiteCart from './components/SideCart/SiteCart'
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// import QuesAns from './components/QuesAns/QuesAns'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import SiteCart from './components/SideCart/SiteCart'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import QuesAns from './components/QuesAns/QuesAns'
 
-// function App() {
+function App() {
   
-//   const [readTime, setReadTime]= useState('')
-//   const [blogTitleList, setBlogTitleLists]= useState([]);
+  const [readTime, setReadTime]= useState('')
+  const [blogTitleList, setBlogTitleLists]= useState([]);
 
-//   const handleReadTime= (readingTime)=>{
-//     const previousReadTime=JSON.parse(localStorage.getItem("countReadingTime"));
-//     if(previousReadTime){
-//       const total=previousReadTime+readingTime;
-//       localStorage.setItem("countReadingTime", total);
-//       setReadTime(total)
-//     }
-//     else{
-//       localStorage.setItem("countReadingTime", readingTime);
-//       setReadTime(readingTime)
-//     }
-//   }
+  const handleReadTime= (readingTime)=>{
+    const previousReadTime=JSON.parse(localStorage.getItem("countReadingTime"));
+    if(previousReadTime){
+      const total=previousReadTime+readingTime;
+      localStorage.setItem("countReadingTime", total);
+      setReadTime(total)
+    }
+    else{
+      localStorage.setItem("countReadingTime", readingTime);
+      setReadTime(readingTime)
+    }
+  }
 
-//   const handleBookmarks = (arr) =>{
-//     // toast("It was not so Easy bro !! nice !! you added  a blog on Bookmark !!");
-//     const newBlogTitleLists = [...blogTitleList,arr]
-//     setBlogTitleLists(newBlogTitleLists);
-//     toast("Wow so easy !");
+  const handleBookmarks = (arr) =>{
+    // toast("It was not so Easy bro !! nice !! you added  a blog on Bookmark !!");
+    const newBlogTitleLists = [...blogTitleList,arr]
+    setBlogTitleLists(newBlogTitleLists);
+    toast("Wow so easy !");
 
-//   };
+  };
 
-//   return (
-//     <div className="App">
-//       <Header></Header>
-//       <div className='flex'>
-//       <Home
-//       handleReadTime={handleReadTime}
-//       handleBookmarks={handleBookmarks}
-//       ></Home>
-//       <SiteCart
-//       readTime={readTime}
-//       blogTitleList={blogTitleList}
-//       ></SiteCart>
-//       </div>
-//       <QuesAns></QuesAns>
-//       <ToastContainer></ToastContainer>
-//     </div>
-//   )
-// }
+  return (
+    <div className="App">
+      <Header></Header>
+      <div className='flex'>
+      <Home
+      handleReadTime={handleReadTime}
+      handleBookmarks={handleBookmarks}
+      ></Home>
+      <SiteCart
+      readTime={readTime}
+      blogTitleList={blogTitleList}
+      ></SiteCart>
+      </div>
+      <QuesAns></QuesAns>
+      <ToastContainer></ToastContainer>
+    </div>
+  )
+}
 
-// export default App
+export default App
